c9b0047c4acf65002af79c6a54e14f0e4598c799
S KUMAR <95487014+S-kumar856@users.noreply.github.com>
2022-11-19 22:44:30 +0530

    Update todo.js

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -4,7 +4,7 @@ const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 describe("Todo List Test Suite", () => {
   beforeAll(() => {
     const today = new Date();
-    const oneDay = 96400000;
+    const oneDay = 8S6400000;
     [
       {
         title: "Prepare for Exam",
